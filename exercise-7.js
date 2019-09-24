981ca7c8edb2def55f9b7c322b82d799d566127b
daniel evan c <danielevanc2@gmail.com>
2019-09-24 17:33:29 +0700

    done

--- exercise-7.js
+++ exercise-7.js
@@ -1,23 +1,23 @@
 // 1. Menyusun Barisan Bintang
-// var rows1 = 5 ;
+var rows1 = 5 ;
 
-// for(var i = 0 ; i < rows1 ; i++){
-//     console.log('*') ;
-// }
+for(var i = 0 ; i < rows1 ; i++){
+    console.log('*') ;
+}
 
 // 2. Menyusun Barisan Bintang Dengan Nested Looping
-// var rows2 = 3 ;
+var rows2 = 5 ;
 
-// for(var y = 0 ; y < rows2 ; y++){
-//     var hasil = '' ;
-//     for(var x = 0 ; x < rows2 ; x++){
-//         hasil += '*' ;
-//     }
-//     console.log(hasil) ;
-// }
+for(var y = 0 ; y < rows2 ; y++){
+    var hasil = '' ;
+    for(var x = 0 ; x < rows2 ; x++){
+        hasil += '*' ;
+    }
+    console.log(hasil) ;
+}
 
 // 3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
-var rows3 = 3 ;
+var rows3 = 5 ;
 
 for(var r = 0 ; r < rows3 ; r++){
     hasil = '' ;
